7130fec660f61eaf12f2311f997b15607180b756
Nguyen Thuc Nguyen Chuong <63794497+ntnc-se@users.noreply.github.com>
2022-03-15 17:49:46 +0700

    commit

--- src/main/java/com/example/demo/service/UserDetailService.java
+++ src/main/java/com/example/demo/service/UserDetailService.java
@@ -19,8 +19,9 @@ public class UserDetailService implements UserDetailsService {
         // normally, call service implement > call repository > get username and password from db (check if have)
         // this is hardcode to get user info
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-        String new_pass = bCryptPasswordEncoder.encode("06052000");
+        String new_pass = bCryptPasswordEncoder.encode("123456789");
         System.out.println("new pass: " + new_pass);
+
         return new User("chuongntn1", new_pass, new ArrayList<>());
 
     }
